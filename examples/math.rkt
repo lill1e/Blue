dfa65fe5b04a8626692571e351eab7f65aee01a0
stairman06 <36215135+stairman06@users.noreply.github.com>
2026-04-27 14:39:52 -0400

    add: math interpreting example

--- examples/math.rkt
+++ examples/math.rkt
@@ -3,4 +3,29 @@
 (generate math "./math.blue")
 (require racket/pretty)
 
-(pretty-print (math-expr-parse "1 * 2 + 3 * 4"))
+;(pretty-print (math-expr-parse "1 * (2 + 5) + 3 * 4"))
+
+(define (exec node)
+ (match node
+   [(? math-prim?) #:when (equal? (math-prim-variant node) 'left)
+    (string->number (math-prim-val node))]
+   [(? math-prim?) #:when (equal? (math-prim-variant node) 'right)
+    (exec (math-prim-val node))]
+   [(? math-expr?) (exec (math-expr-body node))]
+   [(? math-group?) (exec (math-group-body node))]
+   [(? math-add?) 
+    (match (math-add-op node)
+        [(math-add_op _ "+") (+ (exec (math-add-lhs node)) (exec (math-add-rhs node)))]
+        [(math-add_op _ "-") (- (exec (math-add-lhs node)) (exec (math-add-rhs node)))]
+        [_ (exec (math-add-lhs node))])]
+   [(? math-mul?) 
+    (match (math-mul-op node)
+        [(math-mul_op _ "*") (* (exec (math-mul-lhs node)) (exec (math-mul-rhs node)))]
+        [(math-mul_op _ "/") (/ (exec (math-mul-lhs node)) (exec (math-mul-rhs node)))]
+        [_ (exec (math-mul-lhs node))])]))
+
+(define (run expr)
+    (pretty-print (math-expr-parse expr))
+    (exec (math-expr-parse expr)))
+
+(run "1 * (3 + 2) * 9 + 10")
